2d9ee8c7ae701381ceec6060a164297a1a048b33
Sandor Takacs <taki@alkoholista.hu>
2015-07-22 17:06:54 +0200

    Update check_ldap_hosts.sh

--- ldap/check_ldap_hosts.sh
+++ ldap/check_ldap_hosts.sh
@@ -1,9 +1,9 @@
 export IFS='
 ';
 for i in `netstat -an | sed -E 's/[[:space:]]+/ /g' | grep '[\.:]389 ' | fgrep ESTABLISHED | cut -d ' ' -f 5 | cut -f 1 -d ':'`; do
-    O1=`echo ${i} | cut -f 1-3 -d '.'`
-#    echo ${O1}
+    O1=`echo ${i} | cut -f 1 -d '.'`
+    O2=`echo ${i} | cut -f 2 -d '.'`
+    O3=`echo ${i} | cut -f 3 -d '.'`
     O4=`echo ${i} | cut -f 4 -d '.'`
-#    echo ${O4}
-    printf "%s.%03d\n"  ${O1}  ${O4}
+    printf "%03d.%03d.%03d.%03d\n"  ${O1} ${O2} ${O3} ${O4}
 done | sort -n | uniq -c
